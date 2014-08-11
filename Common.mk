4bf4bc5a579f2f5e4c3a0ae5e29c2e4d9d6682b5
Ryan Salsamendi <rsalsamendi@hotmail.com>
2014-08-11 15:56:14 -0700

    Don't add the makefile list to the compiler command line.

--- Common.mk
+++ Common.mk
@@ -131,7 +131,7 @@ $$($(1)_OBJ_DIR)/$$(basename $$(notdir $$(1))).o: $$(abspath $$($(1)_DIR)$$(1)) 
 ifeq ($$(suffix $$(1)),.c)
 	$(CC) -c $$$$< -o $$$$@ $$($(1)_FINAL_CFLAGS) $$(addprefix -I,$$($(1)_HEADER_DIRS))
 else ifneq ($$(filter $$(suffix $$(1)),.cc .cpp),)
-	$(CXX) -c $$$$< -o $$$$@ $$($(1)_FINAL_CFLAGS) $$(addprefix -I,$$($(1)_HEADER_DIRS)) $(MAKEFILE_LIST)
+	$(CXX) -c $$$$< -o $$$$@ $$($(1)_FINAL_CFLAGS) $$(addprefix -I,$$($(1)_HEADER_DIRS))
 endif # .c
 endif # EXE LIB ARC
 endef # CREATE_SOURCE_RULES
